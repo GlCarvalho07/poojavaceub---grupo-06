7060c54ac088c271f7a28432ab6b88e1bb8a6013
renasrenas2 <renato.moreira.faria@gmail.com>
2025-09-26 11:04:25 -0300

    ItemVenda.java

---
+++ ItemVenda.java
@@ -0,0 +1,19 @@
+// ItemVenda.java
+public class ItemVenda {
+    private Produto produto;
+    private int quantidade;
+    private double subtotal;
+
+    public ItemVenda(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.subtotal = calcularSubtotal();
+    }
+
+    public double calcularSubtotal() {
+        return produto.getPreco() * quantidade;
+    }
+
+    public Produto getProduto() { return produto; }
+    public double getSubtotal() { return subtotal; }
+}
